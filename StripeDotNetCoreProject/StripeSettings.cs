b58c0fafe74d94fd818e4b33c5f8747877443932
Enrique Chan <enrique.nec@gmail.com>
2019-05-20 13:04:44 -0500

    Create StripeSettings.cs

---
+++ StripeDotNetCoreProject/StripeSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StripeDotNetCoreProject
+{
+    public class StripeSettings
+    {
+        public String SecretKey { get; set; }
+        public String PublishableKey { get; set; }
+    }
+}
